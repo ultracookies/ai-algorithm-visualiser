af0df23c56afa4a9336706db6434708baeeab51f
Ismail Ajaz <ismail.ajaz19@gmail.com>
2025-07-24 14:26:24 -0400

    Pass second part of test_connect_event

--- backend/app/core/socket.py
+++ backend/app/core/socket.py
@@ -5,7 +5,7 @@ sio_app = socketio.AsyncServer(async_mode='asgi')
 @sio_app.event
 async def connect(sid, environ):
     print(f"Client connected: {sid}")
-    await sio_app.emit('response1', {'msg': 'Connected to ws server'}, to=sid)
+    await sio_app.emit('response1', {'msg': 'Connected to ws server'})
 
 @sio_app.event
 async def disconnect(sid):
