3808083980cc6551d91f2839070b31695a6fcca1
Ismail Ajaz <ismail.ajaz19@gmail.com>
2025-08-11 14:29:59 -0400

    Implement more thorough testing of nn info delivery. Change data format from JSON to bytes

--- backend/tests/tests_route/test_get_json_file.py
+++ backend/tests/tests_route/test_get_json_file.py
@@ -1,9 +1,16 @@
-from pathlib import Path
-import json
+import hashlib
+
+EXPECTED_FILE_SIZE = 25605
+EXPECTED_FILE_HASH = '8f7f7aface65890bfb583ec83f8598748524fc3e3e8f422bfdf63fb4b3129145'
 
 def test_get_json_file(client):
     response = client.get('rl/vdqn')
     assert response.status_code == 200
-    file_path = Path(__file__).parent / '..' / '..' / 'app' / 'algorithms' / 'rl' / 'vanlla_dqn' / 'vdqn.json'
-    with open(file_path, 'r') as f:
-        assert response.json() == json.load(f)
+
+    content_length = response.headers['Content-Length']
+    assert int(content_length) == EXPECTED_FILE_SIZE
+
+    content_hash = hashlib.sha256(response.content).hexdigest()
+    assert content_hash == EXPECTED_FILE_HASH
+
+    
