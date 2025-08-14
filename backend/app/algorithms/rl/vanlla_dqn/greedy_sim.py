eeacdbeed346a4fc3380398c4be75dba8c49a46c
Ismail Ajaz <ismail.ajaz19@gmail.com>
2025-08-14 10:27:12 -0400

    Separate nn logic from rl simulation script

--- backend/app/algorithms/rl/vanlla_dqn/greedy_sim.py
+++ backend/app/algorithms/rl/vanlla_dqn/greedy_sim.py
@@ -1,26 +1,8 @@
 import torch
-import torch.nn as nn
 import gymnasium as gym
 import numpy as np
 
-class CartPoleNeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        HIDDEN_LAYER_DIMS = 24
-        self.fc = nn.Sequential(
-            nn.Linear(4, HIDDEN_LAYER_DIMS),
-            nn.ReLU(),
-            nn.Linear(HIDDEN_LAYER_DIMS, HIDDEN_LAYER_DIMS),
-            nn.ReLU(),
-            nn.Linear(HIDDEN_LAYER_DIMS, HIDDEN_LAYER_DIMS),
-            nn.ReLU(),
-            nn.Linear(HIDDEN_LAYER_DIMS, 2)
-        )
-
-    def forward(self, observation):
-        return self.fc(observation)
-
-model = CartPoleNeuralNetwork()
+from . import gymenvs_neuralnets
 
 def simulate_cartpole(network_instance: dict, epsilon: float, num_episodes=5, steps=500):
     frames = []
@@ -37,7 +19,7 @@ def simulate_cartpole(network_instance: dict, epsilon: float, num_episodes=5, st
         total_reward = 0
         for _ in range(steps):
             obs_tensor = torch.tensor(obs)
-            q_values = model(obs_tensor)
+            q_values = gymenvs_neuralnets.cartpole_model(obs_tensor)
             greedy_action = torch.argmax(q_values).item()
             action = greedy_action
             if np.random.rand() < epsilon:
