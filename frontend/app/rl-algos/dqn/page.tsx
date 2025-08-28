e2ec842f12cf9d8351021b844bdb1bccfeb20fa1
Ismail Ajaz <ismail.ajaz19@gmail.com>
2025-08-28 19:32:44 -0400

    Fix out of bounds error in effect. Add zoom in effect to all features

--- frontend/app/rl-algos/dqn/page.tsx
+++ frontend/app/rl-algos/dqn/page.tsx
@@ -1,5 +1,8 @@
 import NeuralNetworkFetcher from "./neuralNetworkFetcher";
 
 export default function Page() {
+  <h1 color="white" style={{ fontSize: 30 }}>
+    Deep Q Network
+  </h1>;
   return <NeuralNetworkFetcher />;
 }
